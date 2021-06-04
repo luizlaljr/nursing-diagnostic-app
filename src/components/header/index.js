af8e288a8d673847c032b2d1b290fc75aa962a39
luizlaljr <luizlaljr@gmail.com>
2021-06-03 23:36:55 -0300

    Header added

---
+++ src/components/header/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+import logo from '../../assets/logo.svg'
+
+function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          <div className={styles.image}>
+            <img src={logo} alt="heart with pulse" />
+          </div>
+          <div className={styles.brand}>
+            <div className={styles.pretitle}>Nursing</div>
+            <div className={styles.title}>
+              Diagnos<span>IC</span>
+            </div>
+          </div>
+        </div>
+        <nav>
+          <a href="/">Início</a>
+          <a className={styles.actived} href="">
+            Domínios
+          </a>
+          <a href="">Sobre</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
