9cb382fcc8837cc65d1a0a62e5ccae1e30349058
luizlaljr <luizlaljr@gmail.com>
2021-06-14 15:42:32 -0300

    Attribute text removed

--- src/pages/domain/[id].js
+++ src/pages/domain/[id].js
@@ -69,7 +69,7 @@ function Domain(props) {
   const handleNextButton = (id) => {
     return id < props.domains.length ? (id + 1).toString() : handleFetch()
   }
-  if (loading) return <LottieComponent text="Carregando possíveis diagnósticos..." />
+  if (loading) return <LottieComponent />
   return (
     <>
       <Head>
