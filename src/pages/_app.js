2af98a0b2952a17fddca15a47845b3ace690718d
luizlaljr <luizlaljr@gmail.com>
2021-08-23 16:19:39 -0300

    Config on the app page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Router from 'next/router'
 import { ContextProvider } from '../context/context'
 import LottieComponent from '../components/lottie/index'
@@ -9,37 +9,22 @@ import * as ga from '../lib/ga'
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const start = () => {
-      setLoading(true)
-    }
-    const end = () => {
-      setLoading(false)
-    }
-    Router.events.on('routeChangeStart', start)
-    Router.events.on('routeChangeComplete', end)
-    Router.events.on('routeChangeError', end)
-    return () => {
-      Router.events.off('routeChangeStart', start)
-      Router.events.off('routeChangeComplete', end)
-      Router.events.off('routeChangeError', end)
-    }
-  }, [])
+  const start = () => {
+    setLoading(true)
+  }
+  const end = () => {
+    setLoading(false)
+  }
+  Router.events.on('routeChangeStart', start)
+  Router.events.on('routeChangeComplete', end)
+  Router.events.on('routeChangeError', end)
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    // When the component is mounted, subscribe to router changes
-    // and log those page views
-    Router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [Router.events])
+  const handleRouteChange = (url) => {
+    ga.pageview(url)
+  }
+  // When the component is mounted, subscribe to router changes
+  // and log those page views
+  Router.events.on('routeChangeComplete', handleRouteChange)
 
   return (
     <ContextProvider>
